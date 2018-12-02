86d0d04b38f0a0dc02bdd92e230813bd47c87a7f
Esaray74 <you@example.com>
2018-12-01 21:17:24 -0500

    LIRI first commit

---
+++ keys.js
@@ -0,0 +1,19 @@
+console.log('this is loaded');
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
+
+
+
+exports.bandsInTownKeys = {
+    BITkey: '2801bcc8-8e6e-4a49-b393-133cddddd612'
+}
+
+exports.OMDBKeys = {
+    OMDBkey: '3e0807a7'
+}
+
+
+
